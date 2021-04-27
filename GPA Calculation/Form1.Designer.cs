b22555847b5ca5d3f8bdd62618ebad44c139aa6a
Murad Sarker <sarker24@example.com>
2021-04-27 14:05:13 +0200

    first commit

---
+++ GPA Calculation/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace GPA_Calculation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textGrade = new System.Windows.Forms.TextBox();
+            this.textAvg = new System.Windows.Forms.TextBox();
+            this.textTotal = new System.Windows.Forms.TextBox();
+            this.texComputing = new System.Windows.Forms.TextBox();
+            this.textAccounting = new System.Windows.Forms.TextBox();
+            this.textMarketing = new System.Windows.Forms.TextBox();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.Maroon;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            "Letter              Grade",
+            "",
+            "A                     90-100",
+            "B                     80-90",
+            "C                     70-80",
+            "D                     60-70",
+            "F                      0- 60"});
+            this.listBox1.Location = new System.Drawing.Point(584, 66);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(182, 204);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(594, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Grading Points";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textGrade);
+            this.groupBox1.Controls.Add(this.textAvg);
+            this.groupBox1.Controls.Add(this.textTotal);
+            this.groupBox1.Controls.Add(this.texComputing);
+            this.groupBox1.Controls.Add(this.textAccounting);
+            this.groupBox1.Controls.Add(this.textMarketing);
+            this.groupBox1.Controls.Add(this.textName);
+            this.groupBox1.Location = new System.Drawing.Point(26, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(457, 450);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Student Details";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(80, 336);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(48, 17);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Grade";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(80, 287);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 17);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Average";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 237);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(40, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Total";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(80, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 17);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Computing";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(77, 148);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 17);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Accounting";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(80, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Marketing";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Name";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(313, 385);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(213, 385);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(116, 385);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textGrade
+            // 
+            this.textGrade.ForeColor = System.Drawing.Color.DarkRed;
+            this.textGrade.Location = new System.Drawing.Point(168, 333);
+            this.textGrade.Name = "textGrade";
+            this.textGrade.ReadOnly = true;
+            this.textGrade.Size = new System.Drawing.Size(176, 22);
+            this.textGrade.TabIndex = 7;
+            // 
+            // textAvg
+            // 
+            this.textAvg.ForeColor = System.Drawing.Color.DarkRed;
+            this.textAvg.Location = new System.Drawing.Point(168, 284);
+            this.textAvg.Name = "textAvg";
+            this.textAvg.ReadOnly = true;
+            this.textAvg.Size = new System.Drawing.Size(176, 22);
+            this.textAvg.TabIndex = 6;
+            // 
+            // textTotal
+            // 
+            this.textTotal.ForeColor = System.Drawing.Color.DarkRed;
+            this.textTotal.Location = new System.Drawing.Point(168, 234);
+            this.textTotal.Name = "textTotal";
+            this.textTotal.ReadOnly = true;
+            this.textTotal.Size = new System.Drawing.Size(176, 22);
+            this.textTotal.TabIndex = 5;
+            // 
+            // texComputing
+            // 
+            this.texComputing.Location = new System.Drawing.Point(168, 191);
+            this.texComputing.Name = "texComputing";
+            this.texComputing.Size = new System.Drawing.Size(176, 22);
+            this.texComputing.TabIndex = 4;
+            // 
+            // textAccounting
+            // 
+            this.textAccounting.Location = new System.Drawing.Point(168, 145);
+            this.textAccounting.Name = "textAccounting";
+            this.textAccounting.Size = new System.Drawing.Size(176, 22);
+            this.textAccounting.TabIndex = 2;
+            // 
+            // textMarketing
+            // 
+            this.textMarketing.Location = new System.Drawing.Point(168, 95);
+            this.textMarketing.Name = "textMarketing";
+            this.textMarketing.Size = new System.Drawing.Size(176, 22);
+            this.textMarketing.TabIndex = 1;
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(168, 52);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(176, 22);
+            this.textName.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 573);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textGrade;
+        private System.Windows.Forms.TextBox textAvg;
+        private System.Windows.Forms.TextBox textTotal;
+        private System.Windows.Forms.TextBox texComputing;
+        private System.Windows.Forms.TextBox textAccounting;
+        private System.Windows.Forms.TextBox textMarketing;
+        private System.Windows.Forms.TextBox textName;
+    }
+}
+
---
+++ GPA Calculation/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPA_Calculation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int marketing, accounting, computing, total, avg;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textName.ResetText();
+            textAccounting.ResetText();
+            textMarketing.ResetText();
+            texComputing.ResetText();
+            textTotal.ResetText();
+            textAvg.ResetText();
+            textAvg.ResetText();
+            textGrade.ResetText();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            marketing = int.Parse(textMarketing.Text);
+            accounting = int.Parse(textAccounting.Text);
+            computing = int.Parse(texComputing.Text);
+
+            total = marketing + accounting + computing;
+            textTotal.Text = total.ToString();
+
+            avg = total / 3;
+            textAvg.Text = avg.ToString();
+
+
+            if (avg > 90)
+            {
+                textGrade.Text = "A";
+            }
+            else if(avg>80)
+            {
+                textGrade.Text = "B";
+            }
+
+            else if (avg > 70)
+            {
+                textGrade.Text = "C";
+            }
+            else if (avg > 60)
+            {
+                textGrade.Text = "D";
+            }
+
+            else
+            {
+                textGrade.Text = "F";
+            }
+        }
+    }
+}
